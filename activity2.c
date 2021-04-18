d0fed7642b958c93814e15c6ac40c6ade32f98d6
caseycase5 <caseycase5@gmail.com>
2021-04-18 10:51:16 +0000

    Activity 2 Updated
    Updated to include Mutex and Semaphore

--- activity2.c
+++ activity2.c
@@ -2,19 +2,26 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <semaphore.h>
 
 
 int t1, t2;
 int balance = 0;
+
 // Mutex Variable
 pthread_mutex_t lock;
 
+// Semaphore Variable
+sem_t mutex;
 
 void *deposit(void* arg) {
     int *tNum = (int *)arg;
     
     // Mutex locking
-    pthread_mutex_lock(&lock);
+    //pthread_mutex_lock(&lock);
+    
+    // Semaphore locking
+    sem_wait(&mutex);
     
     printf("Thread %d has started \n", *tNum);
     
@@ -25,13 +32,19 @@ void *deposit(void* arg) {
     printf("Balance after Thread: %d\n", balance);
     
     // Mutex unlocking
-    pthread_mutex_unlock(&lock);
+    //pthread_mutex_unlock(&lock);
+    
+    // Semaphore unlocking
+    sem_post(&mutex);
 }
 
 int main() {
     pthread_t thread1, thread2;
     t1 = 1;
     t2 = 2;
+    
+    // Initializing the Semaphore
+    sem_init(&mutex, 0, 1);
     
     // Creation of 2 Threads
     pthread_create(&thread1, NULL, deposit, (void *)&t1);
@@ -40,6 +53,7 @@ int main() {
     sleep(1);
     
     // Destroying the Mutex
-    pthread_mutex_destroy(&lock);
+    //pthread_mutex_destroy(&lock);
+    sem_destroy(&mutex);
     exit(0);
 }
