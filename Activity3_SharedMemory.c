defed849dfc6120ab1bced9387becc16102734c4
caseycase5 <caseycase5@gmail.com>
2021-04-24 07:34:33 +0000

    Activity 3 Processes

--- Activity3_SharedMemory.c
+++ Activity3_SharedMemory.c
@@ -1,36 +1,134 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <sys/mman.h>
+#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/mman.h>
 
-void* create_shared_memory(size_t size) {
-    int protection = PROT_READ | PROT_WRITE;
-    
-    int visibility = MAP_SHARED | MAP_ANONYMOUS;
-    
-    return mmap(NULL, size, protection, visibility, -1, 0);
+// Buffer
+int MAX = 100;
+int WAKEUP = SIGUSR1;
+int SLEEP = SIGUSR2;
+
+pid_t otherPid;
+
+sigset_t sigSet;
+
+struct CIRCULAR_BUFFER {
+    int count;
+    int lower;
+    int upper;
+    int buffer[100];
 }
 
+struct CIRCULAR_BUFFER* buffer = NULL;
 
-void main() {
-    char parent_message[] = "Written by Parent";
-    char child_message[] = "Written by Child";
+// Sleep Method
+void sleepAndWait() {
+    int nSig;
     
-    void* shmem = create_shared_memory(128);
-    
-    int pid = fork();
-    
-    if(pid == 0) {
-        printf("Child read: %s\n", shmem);
-        memcpy(shmem, child_message, sizeof(child_message));
-        printf("Child wrote: %s\n", shmem);
+    // Sleep until wake up signal received
+    printf("Sleeping..\n");
+    sigwait(&sigSet, &nSig);
+    printf("Awaken Signal\n");
+}
+
+// Wake up Method
+void wakeUp() {
+    kill(otherPid, WAKEUP);
+}
+
+// Gets value from buffer
+int getValue() {
+    int value = buffer->buffer[buffer->lower];
+    printf("Consumer read value of %c\n", value);
+    ++buffer->lower;
+    // Returning buffer to beginning when it reaches the end
+    if(buffer->lower == MAX) {
+        buffer->lower = 0;
     }
-    else {
-        printf("Parent read: %s\n", shmem);
+    --buffer->count;
+    return value;
+}
+
+// Puts a value in the buffer
+void putValue(int value) {
+    // Writes value to buffer
+    buffer->buffer[buffer->upper] = value;
+    printf("Producer stored value of %c\n", buffer->buffer[buffer->upper]);
+    ++buffer->upper;
+    // Resets buffer to start if end is reached
+    if(buffer->upper == MAX) {
+        buffer->upper = 0;
+    }
+    ++buffer->count;
+}
+
+void consumer() {
+    // Setup signals
+    sigemptyset(&sigSet);
+    sigaddset(&sigSet, WAKEUP);
+    sigprocmask(SIG_BLOCK, &sigSet, NULL);
+    
+    printf("Running Child Process...\n");
+    
+    // Reading all characters from buffer
+    int character = 0;
+    do {
+        sleepAndWait();
+        character = getValue();
+    }
+    while(character != 0);
+    
+    printf("Exiting Child Process.\n");
+    _exit(1);
+}
+
+void producer() {
+    int value = 0;
+    printf("Running the Parent Process...\n");
+    
+    char message[10] = "Casey Huz";
+    for(int i = 0; i < strlen(message); i++) {
+        // Puts the values of the string into the buffer
+        putValue(message[i]);
+        wakeUp();
         sleep(1);
-        printf("After 1s, Parent read: %s\n", shmem);
-        memcpy(shmem, parent_message, sizeof(parent_message));
-        printf("Parent read: %s\n", shmem);
     }
+    // Tells child process that the parent has completed
+    putValue(0);
+    wakeUp();
+    
+    printf("Exiting the Parent Process.\n");
+    _exit(1);
+}
+
+int main() {
+    pid_t pid;
+    
+    // Create the Buffer
+    buffer = (struct CIRCULAR_BUFFER*)mmap(0, 100, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
+    buffer->count = 0;
+    buffer->lower = 0;
+    buffer->upper = 0;
+    
+    pid = fork();
+    
+    // Printing an error if PID = -1
+    if(pid == -1) {
+        printf("Error forking.");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Executing Child Process
+    if(pid==0) {
+        otherPid = pid;
+        producer();
+    }
+    // Executing Parent Process
+    else {
+        otherPid = pid;
+        consumer();
+    }
+    return 0;
 }
